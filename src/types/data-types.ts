c050b853afe3b79a527edae656924eaa595fb433
Aleksandr Kopeikin <sundstorm91@gmail.com>
2025-09-10 22:59:06 +0700

    feat: create IProduct and ICart interfaces

--- src/types/data-types.ts
+++ src/types/data-types.ts
@@ -1,5 +1,25 @@
-const agg = () => {
-            return '123'
+export interface IProduct {
+    id: number;
+    title: string;
+    price: number;
+    description: string;
+    category: string;
+    image: string;
+    rating?: {
+        rate: number;
+        count: number;
+    }
 }
 
-export default agg;
+export type TCartItem = {
+    productId: number;
+    quantity: number;
+}
+
+export interface ICart {
+    id: number;
+    userId: number;
+    date: string;
+    products: TCartItem[];
+    __v: number;
+}
