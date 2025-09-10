16718e8b1f30142e62bb7c3b021793e7264af9f5
Aleksandr Kopeikin <sundstorm91@gmail.com>
2025-09-10 23:35:29 +0700

    chore: integrate shadcn/ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import { Spinner } from './components/spinner/spinner';
-
+import { Button } from '@/components/ui/button';
 const App = () => {
-    return <Spinner />;
+  return (
+    <div>
+      <Spinner />
+      <Button>Нажми меня</Button>
+      <Button variant="destructive">Удалить</Button>
+      <Button variant="outline">Отмена</Button>
+    </div>
+  );
 };
 
 export default App;
